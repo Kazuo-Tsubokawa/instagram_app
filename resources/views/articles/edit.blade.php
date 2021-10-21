62330127365b37f8a11272480f3fde2b17d9a26c
Kazuo-Tsubokawa <hiwa1712@gmail.com>
2021-10-21 10:22:55 +0900

    editを一部修正

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -8,20 +8,30 @@
             <figure class="m-3">
                 <div class="row">
                     <div class="col-6">
-                        <img src="{{ $article->image_url }}" width="100%">
-                    </div>
-                    <div class="col-6">
                         <figcaption>
                             <form action="{{ route('articles.update', $article) }}" method="post" id="form">
                                 @csrf
                                 @method('patch')
                                 <div class="mb-3">
-                                    <label for="caption" class="form-label">イメージの説明を入力してください</label>
-                                    <input type="text" name="caption" id="caption" class="form-control" value="{{ old('caption',$article->caption) }}">
+                                    <label class="block text-gray-700 text-sm mb-2">おもひでの写真だよ</label>
+                                    <div class="flex flex-wrap -mx-1 lg:-mx-4 mb-4">
+                                        @foreach ($article->attachments as $attachment)
+                                            <article class="w-full px-4 md:w-1/4 text-xl text-gray-800 leading-normal">
+                                                <img class="w-full mb-2"
+                                                    src="{{ Storage::url('articles/' . $attachment->name) }}" alt="image">
+                                            </article>
+                                        @endforeach
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="caption" class="form-label">説明を入力してね</label>
+                                    <input type="text" name="caption" id="caption" class="form-control"
+                                        value="{{ old('caption', $article->caption) }}">
                                 </div>
                                 <div>
-                                    <label for="info" class="form-label">その他情報を入力してください</label>
-                                    <textarea name="info" id="info" rows="5" class="form-control">{{ old('info', $article->info) }}</textarea>
+                                    <label for="info" class="form-label">詳細・その他など</label>
+                                    <textarea name="info" id="info" rows="5"
+                                        class="form-control">{{ old('info', $article->info) }}</textarea>
                                 </div>
                             </form>
                         </figcaption>
